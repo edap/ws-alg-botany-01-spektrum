ae700eab7a0f319389352e99802b6c9a1c7e483f
edapx <lastexxit@gmail.com>
2017-09-09 15:29:24 +0200

    add

---
+++ 04-lsystems/server.js
@@ -0,0 +1,34 @@
+var http = require('http'),
+url = require('url'),
+path = require('path'),
+fs = require('fs');
+
+var mimeTypes = {
+  "html": "text/html",
+  "mp3":"audio/mpeg",
+  "mp4":"video/mp4",
+  "jpeg": "image/jpeg",
+  "jpg": "image/jpeg",
+  "png": "image/png",
+  "js": "text/javascript",
+  "css": "text/css"};
+
+http.createServer(function(req, res) {
+    var uri = url.parse(req.url).pathname;
+    var filename = path.join(process.cwd(), uri);
+    fs.exists(filename, function(exists) {
+        if(!exists) {
+            console.log("not exists: " + filename);
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.write('404 Not Found\n');
+            res.end();
+            return;
+        }
+        var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
+        res.writeHead(200, {'Content-Type':mimeType});
+
+        var fileStream = fs.createReadStream(filename);
+        fileStream.pipe(res);
+
+    }); //end path.exists
+}).listen(1337);
